d8def177049851f3c51bba50ee303447de5bd140
VlabDevNITK <140144238+VlabDevNITK@users.noreply.github.com>
2023-08-01 14:37:56 +0530

    Add files via upload

--- mainFolder/round-template/experiment/posttest.js
+++ mainFolder/round-template/experiment/posttest.js
@@ -1,141 +1,72 @@
-
 // Don't touch the below code
-
-(function() {
+let currentQuestion;
+(function () {
   function buildQuiz() {
-    // we'll need a place to store the HTML output
-    const output = [];
+    let html;
+    fetch("round-template/experiment/posttest.json").then((res) =>
+      res.json().then((data) => {
+        data.questions.forEach((d, i) => {
+          currentQuestion = d;
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
-      const answers = [];
+          html = `<div class="question"> ${i + 1}.  ${
+            currentQuestion.question
+          }</div> <div class="answers">`;
 
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
+          for (letter in currentQuestion.answers) {
+            html += `<label ><input class="question${
+              i + 1
+            }" type="radio" name="question${
+              i + 1
+            }" value="${letter}">${letter} : ${
+              currentQuestion.answers[letter]
+            }</label>`;
+          }
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-      );
-    });
-
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
+          html += `</div>`;
+          quizContainer.insertAdjacentHTML("beforeend", html);
+        });
+      })
+    );
   }
 
   function showResults() {
-    // gather answer containers from our quiz
     const answerContainers = quizContainer.querySelectorAll(".answers");
 
-    // keep track of user's answers
     let numCorrect = 0;
+    let currentQuestion = [];
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+    fetch("round-template/experiment/pretest.json").then((res) =>
+      res.json().then((data) => {
+        data.questions.forEach((d, i) => {
+          currentQuestion.push(d);
 
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
+          const answerContainer = answerContainers[i];
 
-        // color the answers green
-        //answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
+          answerContainer.querySelectorAll("input").forEach((input) => {
+            if (input.checked) {
+              if (input.value === d.correctAnswer) {
+                numCorrect++;
 
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+                answerContainers[i].style.fontWeight = "bold";
+                answerContainers[i].style.color = "green";
+                return;
+              } else if (input.value !== d.correctAnswer) {
+                answerContainers[i].style.fontWeight = "normal";
+                answerContainers[i].style.color = "red";
+              }
+            }
+          });
+
+          resultsContainer.innerHTML = `${numCorrect} out of ${currentQuestion.length}`;
+        });
+      })
+    );
   }
 
   const quizContainer = document.getElementById("quiz");
   const resultsContainer = document.getElementById("results");
   const submitButton = document.getElementById("submit");
 
-
-// Don't touch the above code
-
-
-
-
-// Write your MCQs here --- Start --- --------------------
-
-  const myQuestions = [
-    {
-      question: "Question here",
-      answers: {
-        a: "Option 1",
-        b: "Option 2",
-        c: "Option 3",
-        d: "Option 4"
-      },
-      correctAnswer: "Correct option (If option a is correct then specify a)"
-    },
-    {
-      question: "Question here",
-      answers: {
-        a: "Option 1",
-        b: "Option 2",
-        c: "Option 3",
-        d: "Option 4"
-      },
-      correctAnswer: "b (If option b is correct then specify b)"
-    },
-    {
-      question: "Question here",
-      answers: {
-        a: "Option 1",
-        b: "Option 2",
-        c: "Option 3",
-        d: "Option 4"
-      },
-      correctAnswer: "Correct option (If option a is correct then specify a)"
-    },
-    {
-      question: "Question here",
-      answers: {
-        a: "Option 1",
-        b: "Option 2",
-        c: "Option 3",
-        d: "Option 4"
-      },
-      correctAnswer: "b (If option b is correct then specify b)"
-    },
-    {
-      question: "Question here",
-      answers: {
-        a: "Option 1",
-        b: "Option 2",
-        c: "Option 3",
-        d: "Option 4"
-      },
-      correctAnswer: "b (If option b is correct then specify b)"
-    } //Dont add comma here
-  ];
-
-// ---------------------------- End -------------------------------
-
-  // display quiz right away
   buildQuiz();
-
-  // on submit, show results
   submitButton.addEventListener("click", showResults);
 })();
